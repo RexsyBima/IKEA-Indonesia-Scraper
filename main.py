fcc43aa699a1df5eb3f34c2170a869530f2054bc
Rexsy Bima <Rexsy.Bimq12@gmail.com>
2023-06-12 17:50:25 +0700

    final version, finished

--- main.py
+++ main.py
@@ -1,115 +1,123 @@
 #please pip install -r requirements.txt to venv
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
 
 import csv
-import functions
+import json
+import time
 
 data = []
 dict_category = {}
+datatempo = []
+urllist = []
 
 """to get and read the html page"""
 driver = webdriver.Edge('/usr/local/bin/msedgedriver') #locate the browser bot for Edge
 driver.set_page_load_timeout(200)
-options = Options()
-options.headless = True 
-driver.implicitly_wait(60)
+driver.implicitly_wait(120)
 
 driver.get("https://www.ikea.co.id/in/produk") #launch the browser bot to the url
 html_content = driver.page_source #get url html file
-functions.output(html_content=html_content) #extract important/target main category, subcategories, and urls inside html, then store it to dictvalues.csv
+with open('seleniumhome.html', 'w') as file:
+    """to write the html, BS, lxml it, then prettify it"""
+    soup = BeautifulSoup(html_content, 'lxml')
+    file.writelines(soup.prettify())
+with open('seleniumhome.html', 'r') as file:
+    """to read seleniumhome.html, BS, lxml it, then prettify it, again... and also to search keywords"""
+    content = file.read()
+    soup = BeautifulSoup(content, 'lxml')
+    divs = soup.find_all("div", {"class": "col-sm-6 col-md-3 col-lg-3 text-left"})
+    for div in divs:
+        # Get the h2 text for the key of the dictionary
+        key = div.find("h2").get_text(strip=True)
+        # Find all a tags within this div and create a dictionary with text as key and href as value
+        values = [{"name": a.get_text(strip=True), "url": "www.ikea.co.id" + a.get("href")} for a in div.find_all("a")]
+        # Append to the dictionary
+        dict_category[key] = values
+#Create a writer object
+with open('dictvalues.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Category", "Name", "URL"])
+    for category, subcategories in dict_category.items():
+        for subcategory in subcategories:
+            writer.writerow([category, subcategory['name'], subcategory['url']])
 
-with open('dictvalues.csv', 'r') as file: #read dictvalues
+with open('seleniumout.html', 'w') as file:
+    pass
+
+with open('dictvalues.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         url = row['URL']
         page = 1
         while True:
-            fix_url = f"https://{url}?sort=RECOMMENDED&page={str(page)}" #repetition for every url and pages until they returned empty None value
+            fix_url = f"https://{url}?sort=SALES&page={str(page)}"
+            print(fix_url)
             driver.get(fix_url)
+            time.sleep(3)
+            print("next line is being executed")
+#            try:
+#                input_elements_present = EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[name="productInfo"]'))
+#                WebDriverWait(driver, 1).until(input_elements_present)
+#            except TimeoutException:
+#                print("Timed out waiting for page to load")
             html_content = driver.page_source
-            soup = BeautifulSoup(html_content, 'lxml')
-            content = soup.find_all('input', {'name' : 'productInfo'})
-            page += 1
-            functions.scrape(html_content=html_content, data = data) #scrape to seleniumout.html (a temp html file), then append the target values to data List (which btw contains List of Dictionaries)
-            if not content: #if value return None or empty, it will break and continue to the next lines of codes 
+            soup = BeautifulSoup(html_content, 'html.parser')
+            test = soup.find_all('input', {'name' : 'productInfo'})
+            item_cards = soup.find_all('input', {'name' : 'productInfo'})
+            for itemcard in item_cards:
+                value_str = itemcard['value']
+                value_dict = json.loads(value_str)
+                data.append(value_dict)
+            item_urls = soup.find_all('div', class_= 'd-flex flex-row') 
+            for item_url in item_urls:
+                urlfix = item_url.find('a')
+                href = f"https://www.ikea.co.id{urlfix.get('href') if urlfix else None}"
+                urllist.append(href)
+            for item, urlfix in zip(data, urllist):
+                item['url'] = urlfix 
+            print(test)
+            if not test:
                 break
-    
-    
-functions.jsonsave(data=data) #to write the data(var) containing list of dictionaries into a json
-functions.editdata(data=data) #to edit each List of data variabel, to a form of dictionary. ex = {'name' : 'productnamexyz', 'brand' : 'brandnamexyz'...color..dimension} 
-functions.csvwrite(data=data) #to write into ikea_products.csv
-
-driver.quit()
-print("done")
-    
-
-"""
-this code below is useless and for educational only
-"""
-    # If you want to convert the string to an actual dictionary (Python's version of a JSON object),
-    # you can use json.loads:
-#    value_dict = json.loads(value_str)
-    # This is the dictionary representation of the JSON data. 
-#    print(value_dict)
-    
-#Todo=get rid of it above
-
-
-#with open('output.json', 'w') as output:
-#    """to write into output.json file (for testing)"""
-#    for name in item_cards:
-#        output.writelines(str(name.input) + '\n')
-#
-#
-## First, read the HTML file
-#with open('output.html', 'r') as file:
-#    html_content = file.read()
-#
-## Parse it with Beautiful Soup
-#soup = BeautifulSoup(html_content, 'html.parser')
-#
-## Find all the input elements
-#
-#input_elements = soup.find_all('input', {'name': 'productInfo'}) 
-#
-#
-## For each input element, extract its value and parse it as JSON
-#with open('output.json', 'w') as file:
-#    for input_element in input_elements:
-#        value_str = input_element['value']
-#        value_dict = json.loads(value_str)
-#        json.dump(value_dict, file, indent=4)
-
-
-"""
-THIS CODE BELOW IS USELESS AND TO USE FOR EDUCATION ONLY
-"""    
-    # or do whatever you want with the dict
-
-# For each input element, extract its value and parse it as a dictionar
-
-#URL = 'https://www.ikea.co.id/in/produk/lemari/lemari-pakaian-modular'
-#
-#def scrape(url):
-#    """to scrape a page source from url"""
-#    response = requests.get(url)
-#    source = response.text
-#    return source
-# 
-#with open('coba.html', 'w') as coba_file:
-#    """save the scraped page source into coba.html"""
-#    coba_file = coba_file.writelines(scrape(url=URL))
-#
-#with open('cobaprettify.html', 'r+') as home_file:
-#    """prettify coba.html"""
-#    content = home_file.read()
-#    soup = BeautifulSoup(content, 'lxml')
-#    home_file.writelines(soup.prettify())
-#    
-#    ikea_html_tags = soup.find_all('h6')
-#    for ikea in ikea_html_tags:
-#        print(ikea.text)
+            else:
+                page += 1
  
+for data_ in data:
+    name_parts = data_['name'].split(' - ')
+    data_['brand'] = name_parts[0]
+    descriptionparts = name_parts[1].split(', ')
+    data_['product_name'] = descriptionparts[0]
+    try:
+        if "cm" in descriptionparts[1]:
+            data_['color'] = 'undefined'
+        elif "cm" not in descriptionparts[1]:
+            data_['color'] = descriptionparts[1]
+    except IndexError:
+        data_['color'] = "undefined"
+    try:
+        if "cm" in descriptionparts[1]:
+            data_['dimension'] = descriptionparts[1]
+        else:
+            data_['dimension'] = descriptionparts[2]
+    except IndexError:
+        data_['dimension'] = "undefined"
+    del data_['name']
     
+with open('output.json', 'w') as json_:
+    """to write the data(var) containing list of dictionaries into a json"""
+    json.dump(data, json_, indent=4)
+
+
+with open('ikea_products.csv', 'w', newline='') as csvfile:
+    # Specify the field names
+    fieldnames = ['product_name', 'brand', 'color', 'dimension', 'id', 'price', 'category', 'variant', 'url']
+    # Create a writer object
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Write the header row
+    writer.writeheader()
+    # Write data rows
+    for row in data:
+        writer.writerow(row)
+#print(data)
+#print(type(data))
+#print(urllist)
